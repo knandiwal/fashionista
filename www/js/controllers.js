7b05f800222d2be6e4c2ee53338cf1c6f771cf58
Kraig Walker <kraig_walker@me.com>
2013-08-07 21:04:23 +0100

    tweak tab bar styles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('Fashionista.controllers', [])
 	// Main Controller (main.html)
     .controller('MainCtrl', ['$scope', function ($scope) {
-    	$scope.pageTitle = "Profile";
+		$scope.pageTitle = "Profile";
         $scope.user = {
 			firstname: "Kraig",
 			lastname: "Walker",
@@ -12,4 +12,21 @@ angular.module('Fashionista.controllers', [])
     // View 2 Controller (needs a better name)
     .controller('ViewCtrl', ['$scope', function ($scope) {
         $scope.status = "Also totally works!";
+    }])
+    // Control the divs on the tab bar
+    .controller('TabCtrl', ['$scope', function ($scope) {
+		var scope = $scope;
+		scope.tabDivs = [
+			{ 'class' : '', name : 'Profile', url : '#' },
+			{ 'class' : '', name : 'The Walk', url : '#' },
+			{ 'class' : '', name : 'Discover', url : '#' }
+		];
+		scope.lastSelectedDiv = -1;
+		scope.setSelected = function(idx) {
+			if(scope.lastSelectedDiv != -1) {
+				scope.tabDivs[scope.lastSelectedDiv].class = '';
+			}
+			scope.lastSelectedDiv = idx;
+			scope.stackedDivs[idx].class = 'classA';
+		};
     }]);
